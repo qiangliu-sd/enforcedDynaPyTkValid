77e8d5a6fe01c3938eabd7e5d60b41c6a1d6d1f7
Qiang Liu <ql12@cornell.edu>
2025-01-14 20:23:54 -0800

    Update CppInPy.py

--- src/CppInPy.py
+++ src/CppInPy.py
@@ -13,16 +13,16 @@ class CppInPy:
     
     # paths from os.path.join() (exe, prm_fn, px_fn) have to pass in separately
     def run(self, exe, code, prm_fn, px_fn):   
-        self.msg.config(text=f"Computing ...")
-        eMsg =lambda obj: obj.stderr if obj.stderr != "" else obj.stdout                  
+        self.msg.config(text="Computing ...")
+        eMsg =lambda obj: obj.stderr if obj.stderr else obj.stdout                  
                
         try:
             spRun = sp.run([exe, code, prm_fn, px_fn], check=True,
                 shell=True, capture_output= True,text=True, cwd=os.getcwd())
             self.rCode = spRun.returncode
-            if self.rCode != 0:
-                self.msg.config(text=f"C++ run ERR: see >err_*.log< in [{self.dLog}]\n{eMsg(spRun)}")
+            if self.success():
+                self.msg.config(text="Computing DONE")             
             else:
-                self.msg.config(text=f"Computing DONE")
+                self.msg.config(text=f"C++ run ERR: see >err_*.log< in [{self.dLog}]\n{eMsg(spRun)}")
         except sp.CalledProcessError as xcp:
             self.msg.config(text=f"C++ run XCP:\n{eMsg(xcp)}")            
